ee6dcd3c176355c8802c39aec72c0ee39bfa6024
Ajetunmobi Anjolaoluwa <abdfashion.2014@gmail.com>
2025-01-01 15:05:23 +0100

    operators and datatypes

---
+++ main.py
@@ -0,0 +1,10 @@
+name = "Jane"
+age = 16
+weight = 45.5
+is_student = True
+print("the datatype for name is: ",type(name))
+print("the datatype for age is: ",type(age))
+print("the datatype for weight is: ",type(weight))
+print("the datatype for is_student is: ",type(is_student))
+print('the new datatype for age is:',type(str(age)))
+print('the new datatype for weight is:',type(int(weight)))
